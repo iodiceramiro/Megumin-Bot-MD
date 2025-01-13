3a95c58d390a357e45f4200288ff6eec9d2825ef
iodiceramiro <ramiroiodice550@gmail.com>
2025-01-13 10:17:02 -0300

    Update owner-add_del_owner.js

--- plugins/owner-add_del_owner.js
+++ plugins/owner-add_del_owner.js
@@ -1,9 +1,11 @@
 
 const handler = async (m, { conn, text, args, usedPrefix, command }) => {
   const datas = global
+  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
+  const tradutor = _translate.plugins.owner_add_del_owner
 
   const why = `${tradutor.texto1[0]} ${usedPrefix + command}* @${m.sender.split('@')[0]}\n*◉ ${usedPrefix + command}* ${m.sender.split('@')[0]}\n*◉ ${usedPrefix + command}* <responder>`;
-  const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : true;
+  const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
   if (!who) return conn.reply(m.chat, why, m, {mentions: [m.sender]});
   switch (command) {
     case 'addowner':
@@ -15,6 +17,12 @@ const handler = async (m, { conn, text, args, usedPrefix, command }) => {
       const numeroAEliminar = who;
       const index = global.owner.findIndex(owner => owner[0] === numeroAEliminar);
       if (index !== -1) {
+        global.owner.splice(index, 1);
+        await conn.reply(m.chat, tradutor.texto3, m);
+      } else {
+        await conn.reply(m.chat, tradutor.texto4, m);
+      }
+      break;
   }
 };
 handler.command = /^(addowner|delowner)$/i;
